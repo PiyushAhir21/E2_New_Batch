462bc3f6a4c9452ca34b066fa52fb029b87be7fc
PiyushAhir21 <vasudevahir21@gmail.com>
2025-04-09 17:02:48 +0530

    DDT done

---
+++ vtiger-crm-framework-m16/src/test/java/extra/GetDataFromExcelFile.java
@@ -0,0 +1,40 @@
+package extra;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class GetDataFromExcelFile {
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+//		create java representation object of the physical file
+		FileInputStream fis = new FileInputStream("C:\\Users\\User\\Desktop\\testScriptDataE2.xlsx");
+		
+//		we'll open workbook in read mode
+		Workbook wb = WorkbookFactory.create(fis);
+		
+//		we'll get access of sheet
+//		Sheet sh = wb.getSheet("leads");
+		
+//		we'll get access of row
+//		Row row = wb.getSheet("leads").getRow(1);
+		
+//		we'll get the access of cell
+		Cell cell = wb.getSheet("leads").getRow(1).getCell(0);
+		
+//		we'll read the data
+		String data = cell.getStringCellValue();
+		System.out.println(data);
+		
+//		double value = cell.getNumericCellValue();
+//		boolean stauts = cell.getBooleanCellValue();
+//		LocalDateTime time = cell.getLocalDateTimeCellValue();
+		
+	}
+}
--- vtiger-crm-framework-m16/src/test/java/leadsTest/CreateLeadsWithPhoneTest.java
+++ vtiger-crm-framework-m16/src/test/java/leadsTest/CreateLeadsWithPhoneTest.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,15 +15,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class CreateLeadsWithPhoneTest {
-	public static void main(String[] args) throws IOException {
-		
-//		Getting data from properties file
-		FileInputStream fis = new FileInputStream("C:\\Users\\User\\Basic_Selenium\\vtiger-crm-framework-m16\\src\\test\\resources\\commonData.properties");
+	public static void main(String[] args) throws IOException, InterruptedException {
+//		getting data from properties file
+		FileInputStream fis = new FileInputStream(
+				"C:\\Users\\User\\git\\E2_new_batch\\vtiger-crm-framework-m16\\src\\test\\resources\\commonData.properties");
 		Properties pObj = new Properties();
 		pObj.load(fis);
+		String BROWSER = pObj.getProperty("bro");
 		String URL = pObj.getProperty("url");
 		String USERNAME = pObj.getProperty("un");
 		String PASSWORD = pObj.getProperty("pwd");
+
+//		Getting data from excel file
+		FileInputStream fis1 = new FileInputStream(
+				"C:\\Users\\User\\git\\E2_new_batch\\vtiger-crm-framework-m16\\src\\test\\resources\\testScriptDataE2.xlsx");
+		Workbook wb = WorkbookFactory.create(fis1);
+		String lastName = wb.getSheet("leads").getRow(1).getCell(0).getStringCellValue();
+		String compName = wb.getSheet("leads").getRow(1).getCell(1).getStringCellValue();
+		String phoneNum = wb.getSheet("leads").getRow(1).getCell(2).getStringCellValue();
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -42,9 +54,9 @@ public class CreateLeadsWithPhoneTest {
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.cssSelector("img[title='Create Lead...']")).click();
 
-		String lastName = "Bahadur";
-		String compName = "lensKart";
-		String phoneNum = "9182784587";
+//		String lastName = "Bahadur";
+//		String compName = "lensKart";
+//		String phoneNum = "9182784587";
 
 		WebElement ln = driver.findElement(By.name("lastname"));
 		ln.sendKeys(lastName);
@@ -82,5 +94,7 @@ public class CreateLeadsWithPhoneTest {
 
 		driver.findElement(By.linkText("Sign Out")).click();
 
+		Thread.sleep(2000);
+		driver.close();
 	}
 }
